7e0402cd9b72cd1f98c9aeec83bc7209e256a83b
rashidsarkar <rashidrock558@gmail.com>
2025-11-26 17:58:15 +0600

    refactor: reorganize imports and add getModelByName function in friend service

--- src/app/modules/friend/friend.service.ts
+++ src/app/modules/friend/friend.service.ts
@@ -1,20 +1,11 @@
-import httpStatus from "http-status";
-import AppError from "../../error/appError";
-import { IFriend } from "./friend.interface";
-import Friend from "./friend.model";
-
-const updateUserProfile = async (id: string, payload: Partial<IFriend>) => {
-    if (payload.email || payload.username) {
-        throw new AppError(httpStatus.BAD_REQUEST, "You cannot change the email or username");
-    }
-    const user = await Friend.findById(id);
-    if (!user) {
-        throw new AppError(httpStatus.NOT_FOUND, "Profile not found");
-    }
-    return await Friend.findByIdAndUpdate(id, payload, {
-        new: true,
-        runValidators: true,
-    });
+import httpStatus from 'http-status';
+import AppError from '../../error/appError';
+import { IFriend } from './friend.interface';
+import Friend from './friend.model';
+import Student from '../student/student.model';
+import Teacher from '../teacher/teacher.model';
+const getModelByName = (name: 'Student' | 'Teacher') => {
+  return name === 'Student' ? Student : Teacher;
 };
 
 const FriendServices = { updateUserProfile };
