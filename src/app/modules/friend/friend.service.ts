3cdc7b08eff1569965e5c6ab5a1a54a0fcd080e1
rashidsarkar <rashidrock558@gmail.com>
2025-11-30 14:52:14 +0600

    feat: refactor sendFriendRequest to include user validation and error handling

--- src/app/modules/friend/friend.service.ts
+++ src/app/modules/friend/friend.service.ts
@@ -6,21 +6,80 @@ import { ENUM_REQUEST_STATUS } from '../friendRequest/REQUEST_STATUS';
 import AppError from '../../errors/AppError';
 import httpStatus from '../../utils/httpStatus';
 type UserRole = 'Student' | 'Teacher';
-const getModelByName = (name: UserRole) => {
-  return name === 'Student' ? Student : Teacher;
+
+const findUserById = async (id: string) => {
+  let user = await Student.findById(id);
+  if (user) return { ...user.toObject(), role: 'Student' as UserRole };
+  user = await Teacher.findById(id);
+  if (user) return { ...user.toObject(), role: 'Teacher' as UserRole };
+  return null;
 };
 
-const findUserById = async(id:string)=>{
-    let user = await Student.findById(id)
-    if(user) return {...user.toObject(),role:'Student' as UserRole}
-    user = await Teacher.
-}
+const getUserModel = (role: 'STUDENT' | 'TEACHER') => {
+  return role === 'STUDENT' ? Student : Teacher;
+};
 
 const sendFriendRequest = async (
   senderId: string,
-  senderRole: string,
+  senderRoleFromToken: 'STUDENT' | 'TEACHER',
   receiverId: string,
-) => {};
+) => {
+  const receiver = await findUserById(receiverId);
+  if (!receiver) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Receiver not found');
+  }
+
+  const senderRole: UserRole =
+    senderRoleFromToken === 'STUDENT' ? 'Student' : 'Teacher';
+
+  if (senderId === receiverId && senderRole === receiver.role) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      'Cannot send request to yourself',
+    );
+  }
+
+  const alreadyFriend = await Friend.findOne({
+    $or: [
+      {
+        user: senderId,
+        friend: receiverId,
+      },
+      {
+        user: receiverId,
+        friend: senderId,
+      },
+    ],
+  });
+
+  if (alreadyFriend) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Already friends');
+  }
+  const pendingRequest = await FriendRequest.findOne({
+    $or: [
+      {
+        sender: senderId,
+        receiver: receiverId,
+        status: ENUM_REQUEST_STATUS.PENDING,
+      },
+      {
+        sender: receiverId,
+        receiver: senderId,
+        status: ENUM_REQUEST_STATUS.PENDING,
+      },
+    ],
+  });
+  if (pendingRequest) {
+    throw new AppError(httpStatus.BAD_REQUEST, 'Request already sent');
+  }
+  const request = await FriendRequest.create({
+    sender: senderId,
+    receiver: receiverId,
+    senderModel: senderRole,
+    receiverModel: receiver.role,
+  });
+  return request;
+};
 
 const FriendServices = { sendFriendRequest };
 export default FriendServices;
