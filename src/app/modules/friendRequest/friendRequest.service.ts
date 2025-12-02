5e878c48b3b52f4521c916abd952dd5208b48700
rashidsarkar <rashidrock558@gmail.com>
2025-12-02 14:39:16 +0600

    `Added imports and updated friend request handling logic in FriendRequest service`

--- src/app/modules/friendRequest/friendRequest.service.ts
+++ src/app/modules/friendRequest/friendRequest.service.ts
@@ -1,4 +1,7 @@
+import AppError from '../../errors/AppError';
+import httpStatus from '../../utils/httpStatus';
 import FriendRequest from './friendRequest.model';
+import { ENUM_REQUEST_STATUS } from './REQUEST_STATUS';
 
 const getAllFriendRequests = async () => {
   const result = await FriendRequest.find();
@@ -10,7 +13,16 @@ const handleFriendRequest = async (
   requestId: string,
   profileId: string,
   action: 'accept' | 'reject',
-) => {};
+) => {
+  const request = await FriendRequest.findOne({
+    _id: requestId,
+    receiver: profileId,
+    status: { $ne: ENUM_REQUEST_STATUS.PENDING },
+  });
+  if (!request) {
+    throw new AppError(httpStatus.NOT_FOUND, 'Request not found');
+  }
+};
 
 const FriendRequestServices = { getAllFriendRequests };
 export default FriendRequestServices;
